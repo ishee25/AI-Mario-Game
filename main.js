033fb08e520bc80695ce938031fda2854c888bf3
ishee25 <74249367+ishee25@users.noreply.github.com>
2021-05-20 18:29:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameover = loadSound("gameover.wav");
+	mario_die = loadSound("mariodie.wav");
+	mario_kick = loadSound("kick.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent("canvas");
+
+	instializeInSetup(mario);
+
+	video = createCapture(VIDEO);
+    video.size(800, 400);
+	video.parent("gameConsole");
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+	console.log('Model Loaded!');
+}
+  
+function gotPoses(results){
+	if(results.length > 0){
+	  console.log(results);
+	  nose_x = results[0].pose.nose.x;
+	  nose_y = results[0].pose.nose.y;
+	}
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
